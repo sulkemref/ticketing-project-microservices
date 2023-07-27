65f380010991133ed810834add8b66ac6c98e31f
Kemail-terminal <sulkemref@gmail.com>
2023-07-27 18:00:16 -0500

    GatewayService done

--- gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
@@ -3,6 +3,11 @@ package com.cydeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +16,26 @@ public class GatewayServerApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GatewayServerApplication.class);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/cydeo/user/**")
+                        .filters(f -> f.rewritePath("/cydeo/user/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://user-service"))
+                .route(p -> p
+                        .path("/cydeo/project/**")
+                        .filters(f -> f.rewritePath("/cydeo/project/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://project-service"))
+                .route(p -> p
+                        .path("/cydeo/task/**")
+                        .filters(f -> f.rewritePath("/cydeo/task/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://task-service"))
+                .build();
     }
 }
